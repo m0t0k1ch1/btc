60a6a9cfc0998bc997beadf93cb82991d6e81587
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2016-12-24 23:43:12 +0900

    add OP_DROP

--- op.go
+++ op.go
@@ -6,6 +6,7 @@ const (
 	OP_PUSHDATA1   OP = 0x4c
 	OP_PUSHDATA2   OP = 0x4d
 	OP_PUSHDATA4   OP = 0x4e
+	OP_DROP        OP = 0x75
 	OP_DUP         OP = 0x76
 	OP_EQUALVERIFY OP = 0x88
 	OP_HASH160     OP = 0xa9
@@ -13,9 +14,11 @@ const (
 )
 
 var opCodeMap = map[OP]string{
+	OP_0:           "OP_0",
 	OP_PUSHDATA1:   "OP_PUSHDATA1",
 	OP_PUSHDATA2:   "OP_PUSHDATA2",
 	OP_PUSHDATA4:   "OP_PUSHDATA4",
+	OP_DROP:        "OP_DROP",
 	OP_DUP:         "OP_DUP",
 	OP_EQUALVERIFY: "OP_EQUALVERIFY",
 	OP_HASH160:     "OP_HASH160",
