f88b99491f198f306dfd6620828dfb9a8a0426b8
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2017-03-10 19:28:46 +0900

    refactoring - script_test.go

--- script_test.go
+++ script_test.go
@@ -2,30 +2,46 @@ package btctx
 
 import (
 	"encoding/hex"
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
+type scriptMappingTestCase struct {
+	hex     string
+	asm     string
+	pkh     string
+	address string
+}
+
+var (
+	scriptMappingTestCases = []scriptMappingTestCase{
+		{
+			"76a914cbc222711a230ecdd9a5aa65b61ed39c24db2b3488ac",
+			"OP_DUP OP_HASH160 cbc222711a230ecdd9a5aa65b61ed39c24db2b34 OP_EQUALVERIFY OP_CHECKSIG",
+			"cbc222711a230ecdd9a5aa65b61ed39c24db2b34",
+			"mz6L2hYM8jPR5nhH6kEsc3DQFiSDA1Jqpa",
+		},
+	}
+)
+
 func TestScriptMapping(t *testing.T) {
 	UseTestnet()
 
-	scriptHex := "76a914cbc222711a230ecdd9a5aa65b61ed39c24db2b3488ac"
+	for _, testCase := range scriptMappingTestCases {
+		pubKeyHashBytes, err := hex.DecodeString(testCase.pkh)
+		require.NoError(t, err)
 
-	pubKeyHash := "cbc222711a230ecdd9a5aa65b61ed39c24db2b34"
-	pubKeyHashBytes, err := hex.DecodeString(pubKeyHash)
-	require.NoError(t, err)
-
-	script, err := NewScriptFromHex(scriptHex)
-	require.NoError(t, err)
-	assert.Equal(t, script.Hex, scriptHex)
-	assert.Equal(t, script.Asm, fmt.Sprintf("OP_DUP OP_HASH160 %s OP_EQUALVERIFY OP_CHECKSIG", pubKeyHash))
-	assert.Equal(t, len(script.Addresses), 1)
-	assert.Equal(t, script.Addresses, []string{"mz6L2hYM8jPR5nhH6kEsc3DQFiSDA1Jqpa"})
-	assert.Equal(t, len(script.Data), 1)
-	assert.Equal(t, script.Data, [][]byte{pubKeyHashBytes})
+		script, err := NewScriptFromHex(testCase.hex)
+		require.NoError(t, err)
+		assert.Equal(t, script.Hex, testCase.hex)
+		assert.Equal(t, script.Asm, testCase.asm)
+		assert.Equal(t, len(script.Addresses), 1)
+		assert.Equal(t, script.Addresses, []string{testCase.address})
+		assert.Equal(t, len(script.Data), 1)
+		assert.Equal(t, script.Data, [][]byte{pubKeyHashBytes})
+	}
 }
 
 // ==================================================
