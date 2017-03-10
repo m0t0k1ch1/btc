507469f239103a9104b5c992954274fc2c0622d8
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2017-03-10 19:38:42 +0900

    add test case

--- script_test.go
+++ script_test.go
@@ -1,7 +1,6 @@
 package btctx
 
 import (
-	"encoding/hex"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -9,19 +8,25 @@ import (
 )
 
 type scriptMappingTestCase struct {
-	hex     string
-	asm     string
-	pkh     string
-	address string
+	hex       string
+	asm       string
+	addresses []string
+	data      [][]byte
 }
 
 var (
 	scriptMappingTestCases = []scriptMappingTestCase{
 		{
+			"",
+			"",
+			[]string{},
+			[][]byte{},
+		},
+		{
 			"76a914cbc222711a230ecdd9a5aa65b61ed39c24db2b3488ac",
 			"OP_DUP OP_HASH160 cbc222711a230ecdd9a5aa65b61ed39c24db2b34 OP_EQUALVERIFY OP_CHECKSIG",
-			"cbc222711a230ecdd9a5aa65b61ed39c24db2b34",
-			"mz6L2hYM8jPR5nhH6kEsc3DQFiSDA1Jqpa",
+			[]string{"mz6L2hYM8jPR5nhH6kEsc3DQFiSDA1Jqpa"},
+			[][]byte{[]byte{203, 194, 34, 113, 26, 35, 14, 205, 217, 165, 170, 101, 182, 30, 211, 156, 36, 219, 43, 52}},
 		},
 	}
 )
@@ -30,17 +35,14 @@ func TestScriptMapping(t *testing.T) {
 	UseTestnet()
 
 	for _, testCase := range scriptMappingTestCases {
-		pubKeyHashBytes, err := hex.DecodeString(testCase.pkh)
-		require.NoError(t, err)
-
 		script, err := NewScriptFromHex(testCase.hex)
 		require.NoError(t, err)
 		assert.Equal(t, script.Hex, testCase.hex)
 		assert.Equal(t, script.Asm, testCase.asm)
-		assert.Equal(t, len(script.Addresses), 1)
-		assert.Equal(t, script.Addresses, []string{testCase.address})
-		assert.Equal(t, len(script.Data), 1)
-		assert.Equal(t, script.Data, [][]byte{pubKeyHashBytes})
+		assert.Equal(t, len(script.Addresses), len(testCase.addresses))
+		assert.Equal(t, script.Addresses, testCase.addresses)
+		assert.Equal(t, len(script.Data), len(testCase.data))
+		assert.Equal(t, script.Data, testCase.data)
 	}
 }
 
